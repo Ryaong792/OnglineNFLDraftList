dd459c100f462c3dbe9df8a782f1fc3069ec169d
ryaong792 <ryan.ongchoa@libertymutual.com>
2017-06-05 10:30:50 -0400

    Fix box sizing for cards
    Box sizing was not the same height.
    edit box sizing to 450px;

--- assets/scripts/landing/cards.js
+++ assets/scripts/landing/cards.js
@@ -1,22 +1,22 @@
-$(document).ready(function() {
+'use strict'
 
-  var stack1 = $('#stack1');
+$(document).ready(function () {
+  const stack1 = $('#stack1')
 
-  stack1.removeClass('start');
+  stack1.removeClass('start')
 
-  stack1.hammer().on('tap', function(event) {
-    stack1.addClass('hover');
-    event.stopPropagation();
-  });
+  stack1.hammer().on('tap', function (event) {
+    stack1.addClass('hover')
+    event.stopPropagation()
+  })
 
+  $(document).hammer().on('tap', function (event) {
+    stack1.removeClass('hover')
+    $('.card').removeClass('hover')
+  })
 
-  $(document).hammer().on('tap', function(event) {
-    stack1.removeClass('hover');
-    $('.card').removeClass('hover');
-  });
-
-  $('.card').hammer().on('tap', function(event) {
-      $('.card').removeClass('hover');
-      $(this).addClass('hover');
-  });
-});
+  $('.card').hammer().on('tap', function (event) {
+    $('.card').removeClass('hover')
+    $(this).addClass('hover')
+  })
+})
