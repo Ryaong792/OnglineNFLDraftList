706e5ce9ab7dfd3d2817d2ecc5b9e3776ee22ea5
ryaong792 <ryan.ongchoa@libertymutual.com>
2017-06-05 15:10:36 -0400

    Completed the QB handlebars
    Able to get QBS list with handlebars
    Able to get Draft button working to add to DRAFT list.
    Attempted to try to put a data-table plugin.  But failed will attempt
    later.
    
    Create tables area for handlebars scss.

--- assets/scripts/landing/cards.js
+++ assets/scripts/landing/cards.js
@@ -2,21 +2,20 @@
 
 $(document).ready(function () {
   const stack1 = $('#stack1')
-
   stack1.removeClass('start')
 
-  stack1.hammer().on('tap', function (event) {
-    stack1.addClass('hover')
-    event.stopPropagation()
-  })
+  // stack1.on('tap', function (event) {
+  //   stack1.addClass('hover')
+  //   event.stopPropagation()
+  // })
 
-  $(document).hammer().on('tap', function (event) {
-    stack1.removeClass('hover')
-    $('.card').removeClass('hover')
-  })
-
-  $('.card').hammer().on('tap', function (event) {
-    $('.card').removeClass('hover')
-    $(this).addClass('hover')
-  })
+  // $(document).on('tap', function (event) {
+  //   stack1.removeClass('hover')
+  //   $('.card').removeClass('hover')
+  // })
+  //
+  // $('.card').on('tap', function (event) {
+  //   $('.card').removeClass('hover')
+  //   $(this).addClass('hover')
+  // })
 })
--- assets/scripts/landing/events.js
+++ assets/scripts/landing/events.js
@@ -17,10 +17,6 @@ const addHandlers = () => {
 }
 
 
-
-
-
-
 module.exports = {
   addHandlers
 
